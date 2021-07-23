97932bd07a0a9b75d225f3bda297d2377511040f
Sudowiec <lalarsek@gmail.com>
2021-07-24 00:41:42 +0200

    Another refactor

--- AoC-2017-01.py
+++
@@ -1,9 +0,0 @@
-inp = list(map(int, input()))
-gap = int(len(inp) / 2)
-sum_of_digits = 0
-for i in range(len(inp)):
-    if inp[i] == inp[(i + gap) % len(inp)]:
-        sum_of_digits += inp[i]
-# if inp[0] == inp[len(inp) - 1]:
-    # sum_of_digits += inp[0]
-print(sum_of_digits)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Advent Of Code Repository
+# advent of codee
 _TG 2021_
+adventofcode.com
