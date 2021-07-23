82641adeee07754833ebd07dd541f30a397492d3
Sudowiec <lalarsek@gmail.com>
2021-07-24 01:28:54 +0200

    Finished with lvl 2

--- 2017/15.py
+++ 2017/15.py
@@ -1,5 +1,5 @@
-ASTART = 65
-BSTART = 8921
+ASTART = 679
+BSTART = 771
 FACTORA = 16807
 FACTORB = 48271
 DIVISION = 2147483647
@@ -7,10 +7,22 @@ DIVISION = 2147483647
 vala = ASTART
 valb = BSTART
 counter = 0
-for i in range(40000000):
-    tocount = True
-    vala = (vala * FACTORA) % DIVISION
-    valb = (valb * FACTORB) % DIVISION
-    valbina = bin(vala)[2:]
-    valbinb = bin(valb)[2:]
+to_compare_a = []
+to_compare_b = []
+while True:
+    if len(to_compare_a) < 5000000:
+        vala = (vala * FACTORA) % DIVISION
+        if int(str(vala)[-2:]) % 4 == 0:
+            to_compare_a.append(bin(vala)[-16:])
+    if len(to_compare_b) < 5000000:
+        valb = (valb * FACTORB) % DIVISION
+        if int(str(valb)[-3:]) % 8 == 0:
+            to_compare_b.append(bin(valb)[-16:])
+    if len(to_compare_a) >= 5000000 and len(to_compare_b) >= 5000000:
+        break
+
+
+for i in range(5000000):
+    if to_compare_a[i] == to_compare_b[i]:
+        counter += 1
 print(counter)
