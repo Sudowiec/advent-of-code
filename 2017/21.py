fef240db77af50263ed7574aa519e59522e3b5fd
Sudowiec <lalarsek@gmail.com>
2021-07-25 02:56:14 +0200

    lvl 1, rulesets

--- 2017/21.py
+++ 2017/21.py
@@ -1,11 +1,17 @@
+THRESHOLD = 1
+ITERATIONS = 5
+
+def checkSize(tile):
+    tp = tile.split("/")
+    return len(tp[0])
+
 def printTile(tile):
     tp = tile.split("/")
     for i in tp:
         print(i)
 
 def flipTile(tile, flip):
-    tp = tile.split("/")
-    sidelen = len(tp[0])
+    sidelen = checkSize(tile)
     rttile = []
     for i in range(sidelen):
         rttile.append(list("-" for j in range(sidelen)))
@@ -20,8 +26,7 @@ def flipTile(tile, flip):
     return "/".join(rttile)
 
 def rotateTile(tile):
-    tp = tile.split("/")
-    sidelen = len(tp[0])
+    sidelen = checkSize(tile)
     rttile = []
     for i in range(sidelen):
         rttile.append(list("-" for j in range(sidelen)))
@@ -34,8 +39,24 @@ def rotateTile(tile):
         rttile[i] = "".join(rttile[i])
     return "/".join(rttile)
 
-
-
-tile = ".#./..#/###"
+tile = ".#.#/..##/####/.#.#"
 printTile(tile)
 
+rules23 = {}
+rules34 = {}
+indx = 0
+while True:
+    try:
+        inp = input().split(" => ")
+    except EOFError:
+        break
+    if indx < THRESHOLD:
+        rules23[inp[0]] = inp[1]
+    else:
+        rules34[inp[0]] = inp[1]
+    indx += 1
+
+for iter in range(ITERATIONS):
+    size = checkSize(tile)
+    if size % 2 == 0:
+        difer = 2
