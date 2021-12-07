3cb5611ceec32a461edaaae3557fc53251542c4e
Sudowiec <lalarsek@gmail.com>
2021-12-07 15:32:04 +0100

    upload

---
+++ 2017/22.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+SQUARE = 25
+carrierpos = "0,0"
+carrierdir = "u"
+grid = defaultdict(lambda : ".")
+right = {"r" : "d", "d" : "l", "l" : "u", "u" : "r"}
+left = {"r" : "u", "d" : "r", "l" : "d", "u" : "l"}
+
+# init
+counter = 0
+y = -(SQUARE // 2)
+while True:
+    try:
+        inp = list(input())
+    except EOFError:
+        break
+    x = -(SQUARE // 2)
+    for i in range(SQUARE):
+        grid[str(x) + "," + str(y)] = inp[i]
+        x += 1
+    y += 1
+
+# main loop
+for time in range(10000):
+    # turn the carrier
+    if grid[carrierpos] == "#":
+        carrierdir = right[carrierdir]
+    else:
+        carrierdir = left[carrierdir]
+
+    # infecting / cleaning
+    if grid[carrierpos] == "#":
+        grid[carrierpos] = "."
+    else:
+        grid[carrierpos] = "#"
+        counter += 1
+
+    # move the carrier
+    x = int(carrierpos.split(",")[0])
+    y = int(carrierpos.split(",")[1])
+    if carrierdir == "u":
+        y -= 1
+    elif carrierdir == "d":
+        y += 1
+    elif carrierdir == "r":
+        x += 1
+    elif carrierdir == "l":
+        x -= 1
+    carrierpos = str(x) + "," + str(y)
+print(counter)
+
