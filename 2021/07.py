0ff376bc8333f03994dc56fe03c871a932fb8a55
Sudowiec <lalarsek@gmail.com>
2021-12-07 06:10:01 +0100

    lvl 2

--- 2021/07.py
+++ 2021/07.py
@@ -1,11 +1,12 @@
 crabs = list(map(int, input().split(",")))
 highest = max(crabs)
 
-less = 999999
+less = 99999999
 for goal in range(0, highest + 1):
     fuel = 0
     for i in crabs:
-        fuel += abs(i - goal)
+        steps = abs(i - goal)
+        fuel += int(((2 + (steps - 1)) / 2) * steps)
     if fuel < less:
         less = fuel
 print(less)
