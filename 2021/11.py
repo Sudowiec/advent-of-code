d3a87ede051add729dfad82fa36bf810bc9a9152
Tymek <lalarsek@gmail.com>
2021-12-12 20:14:05 +0100

    lvl 1

---
+++ 2021/11.py
@@ -0,0 +1,48 @@
+SIZE = 10
+ITER = 100
+octopuses = [["#" for i in range(12)]]
+while True:
+    try:
+        inp = list(map(int, list(input())))
+    except EOFError:
+        break
+    inp.insert(0, "#")
+    inp.append("#")
+    octopuses.append(inp)
+octopuses.append(["#" for i in range(12)])
+print(octopuses)
+
+flashcount = 0
+for index in range(ITER):
+    queue = []
+    for y in range(1, SIZE + 1):
+        for x in range(1, SIZE + 1):
+            queue.append([y, x])
+
+    while len(queue) > 0:
+        q = queue.pop(0)
+        y = q[0]
+        x = q[1]
+        if octopuses[y][x] == "#" or octopuses[y][x] == 10:
+            continue
+        octopuses[y][x] += 1
+        if octopuses[y][x] == 10:
+            flashcount += 1
+            queue.extend([
+                [y - 1, x],
+                [y - 1, x + 1],
+                [y, x + 1],
+                [y + 1, x + 1],
+                [y + 1, x],
+                [y + 1, x - 1],
+                [y, x - 1],
+                [y - 1, x - 1]
+            ])
+    for y in range(1, SIZE + 1):
+        for x in range(1, SIZE + 1):
+            if octopuses[y][x] == 10:
+                octopuses[y][x] = 0
+    for i in octopuses:
+        print("".join(map(str, i)))
+    print()
+print(flashcount)
