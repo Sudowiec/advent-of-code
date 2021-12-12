d8cc0ad4815b01a4e02ab5afe5c74128847a1cb9
Tymek <lalarsek@gmail.com>
2021-12-12 15:48:11 +0100

    Lvl 2

--- 2021/09.py
+++ 2021/09.py
@@ -32,3 +32,31 @@ for y in range(1, height + 1):
         lowpoints.append(num + 1)
         lowpoints_coords.append(str(y) + "," + str(x))
 
+# finding basins recursion
+def basin(matrix, x, y):
+    counter = 1
+    num = matrix[y][x]
+    bt.append([y, x])
+    if num < matrix[y + 1][x] and matrix[y + 1][x] < 9 and [y+1, x] not in bt:
+        counter += basin(matrix, x, y + 1)
+    if num < matrix[y - 1][x] and matrix[y - 1][x] < 9 and [y-1, x] not in bt:
+        counter += basin(matrix, x, y - 1)
+    if num < matrix[y][x + 1] and matrix[y][x + 1] < 9 and [y, x+1] not in bt:
+        counter += basin(matrix, x + 1, y)
+    if num < matrix[y][x - 1] and matrix[y][x - 1] < 9 and [y, x-1] not in bt:
+        counter += basin(matrix, x - 1, y)
+    return counter
+
+basins = []
+global bt
+bt = []
+for i in lowpoints_coords:
+    x = int(i.split(",")[1])
+    y = int(i.split(",")[0])
+    basins.append(basin(seafloor, x, y))
+basins = sorted(basins)
+print(basins)
+mul = 1
+for i in range(3):
+    mul *= basins.pop()
+print(mul)
