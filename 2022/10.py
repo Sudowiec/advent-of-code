0d55e0ef33b22b62c2d2888ac3a3f7ce77b131c4
Sudowiec <lalarsek@gmail.com>
2023-05-29 02:09:06 +0200

    Part 2 (very cool)

--- 2022/10.py
+++ 2022/10.py
@@ -14,12 +14,31 @@ s = 0
 for i in range(len(stack)):
     if stack[i] == "noop":
         if not ((i - 19) % 40):
-            print(i + 1, x)
             s += (i + 1) * x
         continue
     else:
         if not ((i - 19) % 40):
-            print(i + 1, x)
             s += (i + 1) * x
         x += int(stack[i].split(" ")[1])
-print(s)
+print("Sum:", s)
+
+# Part 2
+x = 1
+screen = []
+curRow = []
+for i in range(len(stack)):
+    curPos = i % 40
+    if stack[i] == "noop":
+        plus = 0
+    else:
+        plus = int(stack[i].split(" ")[1])
+    if (curPos == x - 1) or (curPos == x) or (curPos == x + 1):
+        curRow.append("#")
+    else:
+        curRow.append(".")
+    if len(curRow) == 40:
+        screen.append(curRow)
+        curRow = []
+    x += plus
+for i in screen:
+    print("".join(i))
