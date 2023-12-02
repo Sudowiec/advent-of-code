d31635bc0d87b0c9743d526166b9b2c729eafa30
Sudowiec <lalarsek@gmail.com>
2023-12-02 07:25:11 +0100

    Part 2 done

--- 2023/01.py
+++ 2023/01.py
@@ -1,9 +1,15 @@
 import os, sys
 
-def isNumber(inp):
-    if inp in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-        return True
-    return False
+PATTERN = {"one" : "1", "two" : "2", "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9"}
+NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
 
 s = 0
 f = open(os.path.join(sys.path[0], "01.txt"), "r")
@@ -11,13 +17,15 @@ while True:
     l = f.readline().strip()
     if l == "":
         break
-    for i in l:
-        if isNumber(i):
-            fs = i
-            break
-    for i in range(len(l) - 1, -1, -1):
-        if isNumber(l[i]):
-            sd = l[i]
-            break
-    s += int(fs + sd)
+    nums = {}
+    for i in PATTERN:
+        found = list(find_all(l, i))
+        for j in found:
+            nums[j] = PATTERN[i]
+    for i in NUMBERS:
+        found = list(find_all(l, i))
+        for j in found:
+            nums[j] = i
+    num = int(nums[min(nums.keys())] + nums[max(nums.keys())])
+    s += num
 print(s)
