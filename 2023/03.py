e4a60c114579cecebaac5623f463cd037eccace4
Sudowiec <lalarsek@gmail.com>
2023-12-03 08:51:54 +0100

    Part one complete

---
+++ 2023/03.py
@@ -0,0 +1,66 @@
+import os, sys
+
+NUMS = "0123456789"
+LENGTH = 140
+
+matrix = []
+
+def getNumber(turn, x, y):
+    px = x
+    num = ""
+    if turn == "left":
+        while matrix[y][px] != ".":
+            num = matrix[y][px] + num
+            px -= 1
+    elif turn == "right":
+        while matrix[y][px] != ".":
+            num += matrix[y][px]
+            px += 1
+    elif turn == "both":
+        while matrix[y][px] != ".":
+            num = matrix[y][px] + num
+            px -= 1
+        px = x + 1
+        while matrix[y][px] != ".":
+            num += matrix[y][px]
+            px += 1
+    print(num)
+    return int(num)
+
+f = open(os.path.join(sys.path[0], "03.txt"), "r")
+
+for i in range(LENGTH):
+    matrix.append(["."] + list(f.readline().strip()) + ["."])
+matrix.append(["." for i in range(len(matrix[0]))])
+matrix.insert(0, ["." for i in range(len(matrix[0]))])
+
+for i in matrix:
+    print("".join(i))
+
+s = 0
+for y in range(len(matrix)):
+    for x in range(len(matrix[0])):
+        if matrix[y][x] == "." or matrix[y][x] in NUMS:
+            continue
+        
+        if matrix[y][x - 1] in NUMS: # LEFT
+            s += getNumber("left", x - 1, y)
+        if matrix[y][x + 1] in NUMS: # RIGHT
+            s += getNumber("right", x + 1, y)
+
+        if matrix[y - 1][x] in NUMS: # UP
+            s += getNumber("both", x, y - 1)
+        else:
+            if matrix[y - 1][x - 1] in NUMS: # UP LEFT
+                s += getNumber("left", x - 1, y - 1)
+            if matrix[y - 1][x + 1] in NUMS: # UP RIGHT
+                s += getNumber("right", x + 1, y - 1)
+        
+        if matrix[y + 1][x] in NUMS: # DOWN
+            s += getNumber("both", x, y + 1)
+        else:
+            if matrix[y + 1][x - 1] in NUMS: # DOWN LEFT
+                s += getNumber("left", x - 1, y + 1)
+            if matrix[y + 1][x + 1] in NUMS: # DOWN RIGHT
+                s += getNumber("right", x + 1, y + 1)
+print(s)                
