5a47656ad1e1e8fa534ca4f278bd148e78028270
Sudowiec <lalarsek@gmail.com>
2023-12-04 07:00:58 +0100

    Part 2 done

--- 2023/04.py
+++ 2023/04.py
@@ -4,14 +4,27 @@ f = open(os.path.join(sys.path[0], "04.txt"), "r")
 
 LENGTH = 193
 
-s = 0
+stack = []
+idcount = {}
 
 for i in range(LENGTH):
     inp = f.readline().strip()[7:].split("|")
     first = list(filter(("").__ne__, inp[0].split(" ")))
     second = list(filter(("").__ne__, inp[1].split(" ")))
-    res = 2**(len(set(first) & set(second)) - 1)
-    if res != 0.5:
-        s += res
+    stack.append({i: [first, second]})
+    idcount[i] = 1
+
+for cur in stack:
+    curid = list(cur.keys())[0]
+    res = len(set(cur[curid][0]) & set(cur[curid][1]))
+    inc = idcount[curid]
+    for i in range(res):
+        idcount[curid + i + 1] += inc
+    print(idcount)
+    
+s = 0
+
+for i in idcount:
+    s += idcount[i] 
 
 print(s)
