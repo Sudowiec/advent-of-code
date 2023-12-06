f37b46318d58400322135aa108fc54175a6f1913
Sudowiec <lalarsek@gmail.com>
2023-12-06 06:36:11 +0100

    Part 2 solved

--- 2023/06.py
+++ 2023/06.py
@@ -5,15 +5,16 @@ f = open(os.path.join(sys.path[0], "06.txt"), "r")
 times = list(map(int, list(filter(("").__ne__, f.readline().strip().split(" ")))[1:]))
 distances = list(map(int, list(filter(("").__ne__, f.readline().strip().split(" ")))[1:]))
 
-m = 1
 for t in range(len(times)):
     curtime = times[t]
     curdistance = distances[t]
-    s = 0
+    b = 0
+    e = 0
     for bvel in range(curtime + 1):
-        print(bvel)
         distance = (curtime - bvel) * bvel
         if distance > curdistance:
-            s += 1
-    m *= s
-print(m)
+            b = bvel
+            break
+    e = curtime - b
+    m = e - b + 1
+print(m, b, e)
