6ca36d03117dff41e6a1f1a56507e06b9a5a4cbb
Sudowiec <lalarsek@gmail.com>
2023-12-13 11:41:53 +0100

    Part 2 solved

--- 2023/08.py
+++ 2023/08.py
@@ -1,4 +1,5 @@
 import os, sys
+import math
 
 LENGTH = 726
 
@@ -9,19 +10,25 @@ dirLen = len(dirs)
 f.readline()
 
 graph = {}
+curs = []
 for i in range(LENGTH):
     t = f.readline().strip().split(" ")
     c = t[2].split(", ")
     graph[t[0]] = {"L" : t[2][1:-1], "R" : t[3][:-1]}
+    if t[0][2] == "A":
+        curs.append(t[0])
 
-steps = 0
-cur = "AAA"
-i = 0
-while True:
-    curstep = dirs[i]
-    steps += 1
-    cur = graph[cur][curstep]
-    if cur == "ZZZ":
-        break
-    i = (i + 1) % dirLen
-print(steps)
+multisteps = []
+for c in curs:
+    cur = c
+    steps = 0
+    i = 0
+    while True:
+        curstep = dirs[i]
+        steps += 1
+        cur = graph[cur][curstep]
+        if cur[2] == "Z":
+            break
+        i = (i + 1) % dirLen
+    multisteps.append(steps)
+print(math.lcm(multisteps[0], multisteps[1], multisteps[2], multisteps[3], multisteps[4], multisteps[5]))
