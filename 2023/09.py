ceed5de18f8e20da06210ce274e76c68aed0d573
Sudowiec <lalarsek@gmail.com>
2023-12-13 22:03:42 +0100

    Added part 2

--- 2023/09.py
+++ 2023/09.py
@@ -1,6 +1,7 @@
 import os, sys
 
 LENGTH = 200
+PART = 2
 
 f = open(os.path.join(sys.path[0], "09.txt"), "r")
 
@@ -17,15 +18,25 @@ for i in range(LENGTH):
         for i in range(len(cur) - 1):
             n.append(cur[i + 1] - cur[i])
         cur = n
-    # print(l)
+    print(l)
     curIndex = len(l) - 1
-    l[curIndex].append(0)
-    while curIndex > 0:
-        curIndex -= 1
-        left = l[curIndex][len(l[curIndex]) - 1]
-        down = l[curIndex + 1][len(l[curIndex]) - 1]
-        l[curIndex].append(left + down)
-    # print(l)
-    s += l[0][len(l[0]) - 1]
+    if PART == 1:
+        l[curIndex].append(0)
+        while curIndex > 0:
+            curIndex -= 1
+            left = l[curIndex][len(l[curIndex]) - 1]
+            down = l[curIndex + 1][len(l[curIndex]) - 1]
+            l[curIndex].append(left + down)
+        s += l[0][len(l[0]) - 1]
+        print(l)
+    elif PART == 2:
+        l[curIndex].insert(0, 0)
+        while curIndex > 0:
+            curIndex -= 1
+            right = l[curIndex][0]
+            down = l[curIndex + 1][0]
+            l[curIndex].insert(0, right - down)
+        s += l[0][0]
+        print(l)
 print(s)
     
