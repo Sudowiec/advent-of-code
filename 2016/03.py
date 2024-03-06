ad26df291bd805de053aab16e0a56552a5e7ca82
Sudowiec <lalarsek@gmail.com>
2024-03-06 16:56:10 +0100

    Part 2 solved

--- 2016/03.py
+++ 2016/03.py
@@ -2,9 +2,15 @@ FLENGTH = 1914
 f = open("2016/03.txt")
 count = 0
 
-for i in range(FLENGTH):
-    l = list(map(int, filter(lambda a: a != "", f.readline().strip().split(" "))))
-    c = l.pop(l.index(max(l)))
-    if sum(l) > c:
-        count += 1
+for i in range(0, FLENGTH, 3):
+    rows = []
+    for j in range(3):
+        rows.append(list(map(int, filter(lambda a: a != "", f.readline().strip().split(" ")))))
+    for r in range(3):
+        l = []
+        for k in range(3):
+            l.append(rows[k][r])
+        c = l.pop(l.index(max(l)))
+        if sum(l) > c:
+            count += 1
 print(count)
