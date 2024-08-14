95c14c69005f6054e2aa3b9358423a2d09fdcee6
t.gryszkalis <t.gryszkalis@samsung.com>
2024-08-14 09:08:09 +0200

    Part 2 done

--- 2016/05.py
+++ 2016/05.py
@@ -1,14 +1,15 @@
 import hashlib
 inp = "abbhdwsy"
 ind = 0
-pw = ""
+pw = list("________")
 while True:
     m = hashlib.md5()
     m.update((inp + str(ind)).encode('utf-8'))
     hex_m = m.hexdigest()
     if hex_m[0:5] == '00000':
-        pw += str(hex_m[5])
-        print(pw)
+        if hex_m[5] in "01234567" and pw[int(hex_m[5])] == "_":
+            pw[int(hex_m[5])] = str(hex_m[6])
+            print("".join(pw))
     ind += 1
-    if len(pw) > 7:
+    if "_" not in pw:
         break
