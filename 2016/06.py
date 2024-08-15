470c385c8a27b08e0629b0d2b33bfb5c132b723e
Sudowiec <lalarsek@gmail.com>
2024-08-15 14:28:15 +0200

    Part 2 solved

--- 2016/06.py
+++ 2016/06.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 FILE_NAME = "2016/06.txt"
 FILE_LENGTH = 650
+PART_ONE = False
 
 input_matrix = []
 f = open(FILE_NAME)
@@ -14,12 +15,19 @@ for x in range(len(input_matrix[0])):
     for y in range(len(input_matrix)):
         current_letter = input_matrix[y][x]
         letter_ranks[current_letter] += 1
-    highest_letter = ""
-    highest_presence = 0
-    for letter in letter_ranks:
-        if letter_ranks[letter] > highest_presence:
-            highest_presence = letter_ranks[letter]
-            highest_letter = letter
-    password.append(highest_letter)
+    chosen_letter = ""
+    if PART_ONE:
+        chosen_presence = 0
+        for letter in letter_ranks:
+            if letter_ranks[letter] > chosen_presence:
+                chosen_presence = letter_ranks[letter]
+                chosen_letter = letter
+    else:
+        chosen_presence = 99999999
+        for letter in letter_ranks:
+            if letter_ranks[letter] < chosen_presence:
+                chosen_presence = letter_ranks[letter]
+                chosen_letter = letter
+    password.append(chosen_letter)
     print("".join(password))
 
