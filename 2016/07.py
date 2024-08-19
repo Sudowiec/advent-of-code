12f57394fb52019a38ebe73d2d17b81b5ccbbc4e
t.gryszkalis <t.gryszkalis@samsung.com>
2024-08-19 15:06:36 +0200

    part 2 done

--- 2016/07.py
+++ 2016/07.py
@@ -1,9 +1,14 @@
 FILE_NAME = "2016/07.txt"
 FILE_SIZE = 2000
 
-def is_abba(potential_string):
-    if len(set(potential_string)) != 1:
-        if potential_string[0:2] == "".join(reversed(potential_string[2:4])):
+def is_aba(potential_string):
+    if potential_string[0] == potential_string[2] and potential_string[0] != potential_string[1]:
+        return True
+    return False
+
+def check_aba_match(word_one, word_two):
+    if word_one[0] == word_two[1] and word_two[1] == word_one[2]:
+        if word_two[0] == word_one[1] and word_one[1] == word_two[2]:
             return True
     return False
 
@@ -12,23 +17,24 @@ f = open(FILE_NAME)
 for line_number in range(FILE_SIZE):
     full_line = f.readline().strip().replace("]", "[").split("[")
     outside_brackets = full_line[::2]
+    outside_abas = []
     inside_brackets = full_line[1::2]
-    tls_outside_brackets = False
-    tls_inside_brackets = False
+    inside_abas = []
     for word in outside_brackets:
-        for i in range(0, len(word) - 3):
-            if is_abba(word[i:i + 4]):
-                tls_outside_brackets = True
-                break
-        if tls_outside_brackets:
-            break
+        for i in range(0, len(word) - 2):
+            if is_aba(word[i:i + 3]):
+                outside_abas.append(word[i:i + 3])
     for word in inside_brackets:
-        for i in range(0, len(word) - 3):
-            if is_abba(word[i:i + 4]):
-                tls_inside_brackets = True
-                break
-        if tls_inside_brackets:
+        for i in range(0, len(word) - 2):
+            if is_aba(word[i:i + 3]):
+                inside_abas.append(word[i:i + 3])
+    found_match = False
+    for outside_word in outside_abas:
+        for inside_word in inside_abas:
+            if check_aba_match(outside_word, inside_word):
+                found_match = True
+        if found_match:
             break
-    if tls_outside_brackets and not tls_inside_brackets:
+    if found_match:
         count += 1
 print(count)
