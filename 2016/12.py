ba34c5fab4cbe9ab16eb0199cecdff301fc7a76d
t.gryszkalis <t.gryszkalis@samsung.com>
2024-09-09 15:33:19 +0200

    Animation added

--- 2016/12.py
+++ 2016/12.py
@@ -1,5 +1,11 @@
+import sys, os
+
 FILE_NAME = "2016/12.txt"
 FILE_LENGTH = 23
+
+MOVE_UP = "\033[F"
+COL_RESET = "\033[0;0m"
+COL_GREEN = "\033[0;32m"
 
 instructions = []
 
@@ -13,11 +19,26 @@ for line_number in range(FILE_LENGTH):
             pass
     instructions.append(instruction)
 
-# cpu = {"a" : 0, "b" : 0, "c" : 0, "d" : 0}
+def print_instructions(index, cpu, last = False):
+    to_print = ""
+    for i in range(len(instructions)):
+        if i == index:
+            to_print += f"{COL_GREEN}> {" ".join(list(map(str, instructions[i])))}{COL_RESET}\n"
+        else:
+            to_print += f"  {" ".join(list(map(str, instructions[i])))}\n"
+    to_print += f"\n"
+    for i in cpu:
+        to_print += f"  [{i}]: {cpu[i]}{10 * " "}\n"
+    if not last:
+        to_print += f"{MOVE_UP * (len(instructions) + 5)}"
+    sys.stdout.write(to_print)
+
 cpu = {"a" : 0, "b" : 0, "c" : 1, "d" : 0}
 index = 0
+os.system("cls")
 while index < len(instructions):
     current_instruction = instructions[index]
+    print_instructions(index, cpu)
     if current_instruction[0] == "cpy":
         if type(current_instruction[1]) is int:
             cpu[current_instruction[2]] = current_instruction[1]
@@ -31,4 +52,4 @@ while index < len(instructions):
         if current_instruction[1] == 1 or cpu[current_instruction[1]] != 0:
             index += current_instruction[2] - 1
     index += 1
-print(cpu)
+print_instructions(index, cpu, True)
