6a3f79bf2be99602021a0a330711df4cc195d3d5
Sudowiec <lalarsek@gmail.com>
2024-09-11 20:26:45 +0200

    Part 2 solved

--- 2016/19.py
+++ 2016/19.py
@@ -1,15 +1,20 @@
+from collections import deque
+
 NUMBER_OF_ELVES = 3017957
 
-elves = {}
-for elf_number in range(NUMBER_OF_ELVES):
-    next = elf_number + 1 if elf_number + 1 != NUMBER_OF_ELVES else 0
-    elves[elf_number] = {"next" : next}
+left = deque()
+right = deque()
+for i in range(1, NUMBER_OF_ELVES + 1):
+    if i < (NUMBER_OF_ELVES // 2) + 1:
+        left.append(i)
+    else:
+        right.appendleft(i)
 
-current_elf = 0
-while len(elves) > 1:
-    steal_from = elves[current_elf]["next"]
-    next = elves[steal_from]["next"]
-    elves[current_elf]["next"] = next
-    current_elf = elves[current_elf]["next"]
-    elves.pop(steal_from)
-print(list(elves.keys())[0] + 1)
+while left and right:
+    if len(left) > len(right):
+        left.pop()
+    else:
+        right.pop()
+    right.appendleft(left.popleft())
+    left.append(right.pop())
+print(left[0] or right[0])
