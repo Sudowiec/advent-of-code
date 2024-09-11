73fd615396ecd8a46d2dd78b6373d0f4fab808cd
Sudowiec <lalarsek@gmail.com>
2024-09-11 12:38:48 +0200

    Part 2 done

--- 2016/15.py
+++ 2016/15.py
@@ -8,19 +8,19 @@ f = open(FILE_NAME)
 for line_number in range(FILE_LENGTH):
     inp = f.readline().strip().split(" ")
     discs_start[line_number] = {"positions" : int(inp[3]), "current" : int(inp[11][:-1])}
+discs_start[FILE_LENGTH] = {"positions" : 11, "current" : 0}
 
-def rotate_discs(discs):
+def rotate_discs(discs, times):
     for disc in discs:
-        discs[disc]["current"] = (discs[disc]["current"] + 1) % discs[disc]["positions"]
+        discs[disc]["current"] = (discs[disc]["current"] + times) % discs[disc]["positions"]
 
 start_time = 0
 while True:
     discs = copy.deepcopy(discs_start)
-    for i in range(start_time):
-        rotate_discs(discs)
+    rotate_discs(discs, start_time)
     goes_through = True
     for position in range(len(discs)):
-        rotate_discs(discs)
+        rotate_discs(discs, 1)
         if discs[position]["current"] == 0:
             continue
         goes_through = False
