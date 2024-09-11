febc61dac35401f1b677cad0271117dd173ab35a
Sudowiec <lalarsek@gmail.com>
2024-09-11 13:37:25 +0200

    Part 1 solved

---
+++ 2016/18.py
@@ -0,0 +1,21 @@
+START_ROW = ".^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^...."
+ROWS_TO_ASSESS = 40
+ROW_LENGTH = len(START_ROW)
+
+def tell_tile(left, right):
+    if left != right:
+        return "^"
+    return "."
+
+number_of_safes = 0
+current_row = START_ROW
+for row_number in range(ROWS_TO_ASSESS):
+    print(current_row)
+    number_of_safes += current_row.count(".")
+    new_row = ""
+    for tile_number in range(ROW_LENGTH):
+        left = current_row[tile_number - 1] if tile_number > 0 else "."
+        right = current_row[tile_number + 1] if tile_number < ROW_LENGTH - 1 else "."
+        new_row += tell_tile(left, right)
+    current_row = new_row
+print(number_of_safes)
