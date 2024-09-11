d8fdb9b0ff836e9a778121a65f6256d47bd3c3a0
Sudowiec <lalarsek@gmail.com>
2024-09-11 22:25:13 +0200

    Part 2 solved

--- 2016/21.py
+++ 2016/21.py
@@ -1,8 +1,9 @@
-import sys, time
+import sys, itertools
 
 FILE_NAME = "2016/21.txt"
 FILE_LENGTH = 100
-START_COMBINATION = "abcdefgh"
+LOOKING_FOR = "fbgdceah"
+PATTERN = "abcdefgh"
 
 def swap(string, index_one, index_two):
     string = list(string)
@@ -28,29 +29,38 @@ def move(string, index_one, index_two):
     string.insert(index_two, tmp)
     return "".join(string)
 
+perms = []
+perms = list(itertools.permutations(list(PATTERN)))
+for i in range(len(perms)):
+    perms[i] = "".join(perms[i])
+
+instructions = []
 f = open(FILE_NAME)
-current_combination = START_COMBINATION
-for line_number in range(FILE_LENGTH):
-    time.sleep(0.1)
-    instruction = f.readline().strip().split(" ")
-    if instruction[0] == "swap":
-        if instruction[1] == "position":
-            current_combination = swap(current_combination, int(instruction[2]), int(instruction[5]))
-        else:
-            current_combination = swap(current_combination, current_combination.index(instruction[2]), current_combination.index(instruction[5]))
-    elif instruction[0] == "rotate":
-        if instruction[1] == "based":
-            times = current_combination.index(instruction[6]) + 1
-            times += 1 if times > 4 else 0
-            direction = "right"
-        else:
-            times = int(instruction[2])
-            direction = instruction[1]
-        for i in range(times):
-            current_combination = rotate(current_combination, direction)
-    elif instruction[0] == "reverse":
-        current_combination = reverse(current_combination, int(instruction[2]), int(instruction[4]))
-    elif instruction[0] == "move":
-        current_combination = move(current_combination, int(instruction[2]), int(instruction[5]))
-    sys.stdout.write(f"{current_combination}\r")
-print(current_combination)
+for i in range(FILE_LENGTH):
+    instructions.append(f.readline().strip().split(" "))
+
+for p in perms:
+    current_combination = p
+    for instruction in instructions:
+        if instruction[0] == "swap":
+            if instruction[1] == "position":
+                current_combination = swap(current_combination, int(instruction[2]), int(instruction[5]))
+            else:
+                current_combination = swap(current_combination, current_combination.index(instruction[2]), current_combination.index(instruction[5]))
+        elif instruction[0] == "rotate":
+            if instruction[1] == "based":
+                times = current_combination.index(instruction[6]) + 1
+                times += 1 if times > 4 else 0
+                direction = "right"
+            else:
+                times = int(instruction[2])
+                direction = instruction[1]
+            for i in range(times):
+                current_combination = rotate(current_combination, direction)
+        elif instruction[0] == "reverse":
+            current_combination = reverse(current_combination, int(instruction[2]), int(instruction[4]))
+        elif instruction[0] == "move":
+            current_combination = move(current_combination, int(instruction[2]), int(instruction[5]))
+    if current_combination == LOOKING_FOR:
+        print(p)
+        break
